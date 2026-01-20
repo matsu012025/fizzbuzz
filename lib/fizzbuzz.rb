bea5b2750a37b97f7cfd510662aa021eb5d951c3
matsu <yell01200425@gmail.com>
2026-01-20 22:30:22 +0900

    Fix output Github

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,13 @@
 class Fizzbuzz
   def calculate number
-    if number % 3 == 0 && number % 5 == 0
+    if number.to_s.inclide? '7' #追加
+      'Github'
+    elsif number % 3 == 0 && number % 5 == 0
       'fizzbuzz'
     elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
       'buzz'
-    elsif number.to_s.inclide? '7' #追加
-      'Github'
     else
       number
     end
